56c4c3689ec300efc7d18fc98a043549a4f72b29
Sarmad426 <sarmadrafique040@gmail.com>
2024-01-29 15:29:30 +0500

    Dynamic page template

---
+++ Store/store_content.php
@@ -0,0 +1,7 @@
+<!-- store_content.php -->
+<?php
+// Simulate fetching store content from the database based on the selected store ID
+$storeContent = "This is the content for Store " . $selectedStore . ".";
+echo "<p>$storeContent</p>";
+?>
+
