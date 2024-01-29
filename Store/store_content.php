a42b1ff31c48400166ea95acfcac478c941f9ca5
Sarmad426 <sarmadrafique040@gmail.com>
2024-01-29 16:09:21 +0500

    Renamed the file

--- Store/store_content.php
+++
@@ -1,7 +0,0 @@
-<!-- store_content.php -->
-<?php
-// Simulate fetching store content from the database based on the selected store ID
-$storeContent = "This is the content for Store " . $selectedStore . ".";
-echo "<p>$storeContent</p>";
-?>
-
